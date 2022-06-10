343925c3434c1c774e9a1344b54e40aba4bd4b1d
joymohanta <joymohanta06@gmail.com>
2022-06-10 06:17:43 +0600

    typewriter added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,40 @@
 import React from "react";
 import "./Home.css";
+import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
+import TypeWriterEffect from "react-typewriter-effect";
 
 const Home = () => {
   return (
     <div className="main-page">
       <div className="home-pic">
-        <div>
-          <h1>Hello, Welcome!!</h1>
-          <h1 className="name-title">Joy Mohanta</h1>
+        <div className="font-style">
+          <h2>Welcome!! My Name is</h2>
+          {/* <h1 className="name-title">
+            <TypeWriterEffect
+              startDelay={2000}
+              cursorColor="#fff"
+              multiText={["Joy Mohanta", "Joy Mohanta"]}
+              multiTextDelay={2000}
+              typeSpeed={110}
+              multiTextLoop
+            />
+          </h1> */}
           <p>
             I am a full stack web developer. I have couple of projects done by
             me. <br /> My projects are made of MERN style. If you are interested
             please leave <br /> a message to me below.
           </p>
+          <br /> <br />
+          <a className="button" href="#">
+            Hire Me
+            <ArrowNarrowRightIcon className="icon"></ArrowNarrowRightIcon>
+          </a>
         </div>
         <div className="developer-picture">
-          <img src="https://i.ibb.co/4ffZy8d/stack.jpg" alt="" />
+          <img
+            src="https://res.digital/wp-content/uploads/2019/12/word-image-1.png"
+            alt=""
+          />
         </div>
       </div>
     </div>
